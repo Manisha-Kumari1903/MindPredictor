932c551aaa94342ea4ff4b3685f65e4011a40cee
Manisha Kumari <manimarch19@gmail.com>
2026-03-13 17:14:54 +0530

    Delete mind_predictor/mhp directory

--- mind_predictor/mhp/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TweetAnalysis
-
-@admin.register(TweetAnalysis)
-class TweetAnalysisAdmin(admin.ModelAdmin):
-    list_display  = ('tweet_text', 'result', 'sentiment_label', 'polarity_score', 'analyzed_at')
-    list_filter   = ('result', 'sentiment_label')
-    search_fields = ('tweet_text',)
