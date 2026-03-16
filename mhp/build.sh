6ee8a9b7210131a4db68acf4e9b36627ee1e865a
Manisha Kumari <manimarch19@gmail.com>
2026-03-16 12:11:48 +0530

    Delete mhp directory

--- mhp/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-pip install -r requirements.txt
-python -m nltk.downloader punkt stopwords wordnet punkt_tab averaged_perceptron_tagger
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --no-input
